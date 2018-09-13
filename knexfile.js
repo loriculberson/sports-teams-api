4701166bacb08e8ff22e3ff3a037e85f5b289d45
Lori Culberson <lori_culberson@homedepot.com>
2018-09-12 21:55:17 -0400

    Add knex files

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+// Update with your config settings.
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      database: 'know_clue'
+    }
+  },
+};
