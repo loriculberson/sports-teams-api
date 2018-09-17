d05961ad5fd4023d937918872d1208daaf977cef
Lori Culberson <lori_culberson@homedepot.com>
2018-09-16 23:18:00 -0400

    Remove comments in seed file and add instructions to Readme

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,17 @@
 ### Sports Teams API
 
+#### To Run Migration
+`knex migrate:latest`
+
+#### To Seed the Database
+`knex seed:run`
+
+#### API Endpoints
+GET
+* all US sports teams
+* all NFL teams
+* all MLB teams
+* all NBA teams
 
 Data from 2013 courtesy of: https://github.com/radavis/gametoday/blob/master/db/teams.csv
 
--- seeds/teams.js
+++ seeds/teams.js
@@ -1,41 +1,24 @@
-// const teamsData = require('../data/test_data.csv');
 const csv = require('csv-parser');
 const fs = require('fs');
 const path = require('path');
 
 const teamsCsv = path.join('data', 'sports_teams.csv');
-// const teamsData = fs.createReadStream(teamsCsv)
+
 const createTeams = () => {
   return new Promise((resolve) => {
     const allTeams = [];
     fs.createReadStream(teamsCsv)
     .pipe(csv())
     .on('data', (data) => {
-      //.on adds the event listener
-      //'data' is the event
-      //function(data) is the callback function
         const team = new Team(data.Team, data.City, data.State, data.Venue, data.League)
         allTeams.push(team)
-        // console.log(allTeams);
     })
     .on('end', () => {
-      //resolve the promise with teamsData
       resolve(allTeams);
     })
   })
 }
 
-
-// createTeams() returns a Promise. See line 9
-//the Promise constructor takes a function and that function takes two arguments which are both functions called ==> resolve and reject
-//resolve is a function that takes an argument that can be any type of data (like an array)
-
-//resolve = (teamsData) => {
-  // console.log(teamsData);
-// }
-
-
-//construct a Team object; blueprint
 function Team(team_name, city, state, venue, sport_league){
   this.team_name = team_name;
   this.city = city;
