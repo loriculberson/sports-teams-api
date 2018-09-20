093a2fab4ec1e55e314c33c552f5511f206cb89b
Lori Culberson <lori_culberson@homedepot.com>
2018-09-20 15:54:33 -0400

    Add all teams endpoint

--- routes/teams.js
+++ routes/teams.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
 
-// router.get('/', (req, res) => {
-//   knex
-//     .select('*')
-//     .from('teams')
-//     .then(data => res.status(200).json(data))
-//     .catch(err =>
-//       res.status(404).json({
-//         message:
-//           'The data you are looking for could not be found. Please try again'
-//       })
-//     );
-// });
 router.get('/', (req, res) => {
-  res.status(200).json({ message: 'All Teams API 2013' });
+  knex
+    .select('*')
+    .from('teams')
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+      res.status(404).json({
+        message:
+          'The data you are looking for could not be found. Please try again'
+      })
+    );
 });
+// router.get('/', (req, res) => {
+//   res.status(200).json({ message: 'All Teams API 2013' });
+// });
 module.exports = router;
