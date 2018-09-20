f111db09f68bb5de5a65fb904605e2f15c0268c4
Lori Culberson <lori_culberson@homedepot.com>
2018-09-20 14:59:57 -0400

    Wired /api and /api/teams endpoints

--- routes/teams.js
+++ routes/teams.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+// router.get('/', (req, res) => {
+//   knex
+//     .select('*')
+//     .from('teams')
+//     .then(data => res.status(200).json(data))
+//     .catch(err =>
+//       res.status(404).json({
+//         message:
+//           'The data you are looking for could not be found. Please try again'
+//       })
+//     );
+// });
 router.get('/', (req, res) => {
-  knex
-    .select('*')
-    .from('teams')
-    .then(data => res.status(200).json(data))
-    .catch(err =>
-      res.status(404).json({
-        message:
-          'The data you are looking for could not be found. Please try again'
-      })
-    );
+  res.status(200).json({ message: 'All Teams API 2013' });
 });
+module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 
 const bodyParser = require('body-parser');
 
-const index = require('./routes');
+const index = require('./routes/index');
 const teams = require('./routes/teams');
 
 const app = express();
@@ -12,9 +12,14 @@ const PORT = process.env.PORT || 3000;
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.get('/', (req, res) => res.send('Welcome to the API'))
 
-app.use('/api/', index);
-app.use('/api/', teams);
+app.use('/api', index);
+app.use('/api/teams', teams);
 
-app.listen(3000, () => console.log("Server running on port 3000"))
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
